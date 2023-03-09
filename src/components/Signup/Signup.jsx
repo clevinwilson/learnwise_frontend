ea1eb2778780787d8723dffc7e84a2bbeda43a9d
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-03-09 10:53:14 +0530

    Signup function added

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -15,8 +15,8 @@ function Signup() {
     password:Yup.string()
       .min(6, 'Password must be at least 6 charaters')
       .required('Password is Required'),
-    confirmPassword:Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Password must match')
+    confirmpassword:Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
       .required('Confirm Password is Required ')
   });
 
@@ -25,7 +25,7 @@ function Signup() {
       firstName: '',
       email: '',
       password: '',
-      confirmPassword: ''
+      confirmpassword: ''
     },
     validationSchema:validate,
     onSubmit: (values) => {
@@ -35,7 +35,7 @@ function Signup() {
   })
 
   const handleChange=(event)=>{
-    console.log(event.target.name);
+    console.log(event.target.value);
     formik.setValues((prev) =>{
       const formFields = {...prev};
       formFields[event.target.name]=event.target.value;
@@ -53,18 +53,19 @@ function Signup() {
             <div class="relative mb-6" data-te-input-wrapper-init>
               <input
                 type="text"
-                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 input-box-border"
-                id="exampleFormControlInput2"
+                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-violet-900  input-box-border"
+                style={{ color:"black"}}
+                id="firstName"
                 name='firstName'
                 onChange={() => { handleChange(event)}}
                 placeholder="First Name" />
-              <label
-                for="exampleFormControlInput2"
+              {/* <label
+                for="firstName"
                 class="input-box-lable pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
               >First Name
-              </label>
+              </label> */}
               {formik.touched.firstName && formik.errors.firstName ? (
-                <div>{formik.errors.firstName}</div>
+                <div className='text-red-500'>{formik.errors.firstName}</div>
               ) : null}
             </div>
 
@@ -87,48 +88,60 @@ function Signup() {
             <div class="relative mb-6" data-te-input-wrapper-init>
               <input
                 type="email"
-                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 input-box-border"
+                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200  dark:placeholder:text-violet-900  input-box-border"
                 id="exampleFormControlInput2"
+                style={{ color: "black" }}
                 name='email'
                 onChange={() => { handleChange(event) }}
 
                 placeholder="Email address" />
-              <label
+              {/* <label
                 for="exampleFormControlInput2"
                 class="input-box-lable pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
               >Email address
-              </label>
+              </label> */}
+              {formik.touched.email && formik.errors.email ? (
+                <div className='text-red-500' >{formik.errors.email}</div>
+              ) : null}
             </div>
 
             <div class="relative mb-6" data-te-input-wrapper-init>
               <input
                 type="password"
-                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 input-box-border"
+                className="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200  dark:placeholder:text-violet-900  input-box-border"
                 id="exampleFormControlInput2"
+                style={{ color: "black" }}
                 name='password'
                 onChange={() => { handleChange(event) }}
 
                 placeholder="Password" />
-              <label
+              {/* <label
                 for="exampleFormControlInput2"
                 class="input-box-lable pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
               >Password
-              </label>
+              </label> */}
+              {formik.touched.password && formik.errors.password ? (
+                <div className='text-red-500'>{formik.errors.password}</div>
+              ) : null}
             </div>
             <div class="relative mb-6" data-te-input-wrapper-init>
               <input
                 type="password"
-                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 input-box-border"
+                class="peer  block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200  dark:placeholder:text-violet-900  input-box-border"
                 id="exampleFormControlInput2"
+                style={{ color: "black" }}
                 name='confirmpassword'
                 onChange={() => { handleChange(event) }}
 
                 placeholder="Confirm password" />
-              <label
+              {/* <label
                 for="exampleFormControlInput2"
                 class="input-box-lable pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.7rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
               >Confirm password
-              </label>
+              </label> */}
+              {formik.touched.confirmpassword && formik.errors.confirmpassword ? (
+                <div className='text-red-500'>{formik.errors.confirmpassword}</div>
+              ) : null}
             </div>
 
             <div class="text-center ">
