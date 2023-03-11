7e62008609f0eec2c82f28f87853fe43165ad395
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-03-11 11:58:48 +0530

    admin header navbar updated

--- src/components/AdminHeader/AdminHeader.jsx
+++ src/components/AdminHeader/AdminHeader.jsx
@@ -22,9 +22,11 @@ function AdminHeader() {
                       </svg>
                   </button>
               </div>
-              
-              <a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Sign In</a>
-              <a className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a>
+            
+              <svg className="hidden lg:inline-block lg:ml-auto lg:mr-3  h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <title>Mobile menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
           </nav>
          
     </div>
