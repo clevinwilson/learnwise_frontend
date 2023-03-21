023128f39603587a9d3d959c342154be8de002b6
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-03-21 18:37:53 +0530

    course details page  completed

--- src/pages/user/CourseDetailsPage.jsx
+++ src/pages/user/CourseDetailsPage.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import CourseDetails from '../../components/CourseDetails/CourseDetails';
 import UserHeader from '../../components/UserHeader/UserHeader';
 import Footer from '../../components/UserFooter/UserFooter';
+import BottomNavigation from '../../components/BottomNavigation/BottomNavigation'
 
 function CourseDetailsPage() {
   return (
     <React.Fragment>
         <UserHeader/>
         <CourseDetails/>
-        {/* <Footer/> */}
+      <BottomNavigation />
+        <Footer/>
     </React.Fragment>
   )
 }
