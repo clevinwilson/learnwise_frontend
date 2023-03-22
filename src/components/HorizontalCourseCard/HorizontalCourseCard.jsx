80b4f07e0a21bd4a01e29770bd1b799876aee66f
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-03-22 10:41:06 +0530

    my enrollments page created

--- src/components/HorizontalCourseCard/HorizontalCourseCard.jsx
+++ src/components/HorizontalCourseCard/HorizontalCourseCard.jsx
@@ -9,8 +9,8 @@ function horizontalCourseCard() {
             <div className="flex justify-center mt-4 sm:mx-10 m-3">
                 <a href="#" className="flex p-4 w-full max-w-screen-lg hover:bg-violet-50 flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <img className="rounded mt-4 sm:mt-0" src="https://img-c.udemycdn.com/course/240x135/2887266_c696_5.jpg" alt />
-                    <div className="flex flex-col justify-between mt-2 sm:0 p-4 leading-normal">
-                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">The Complete 2023 Web Development
+                    <div className="flex flex-col ml-0 sm:ml-3 justify-between mt-2 sm:0 p-4 leading-normal">
+                        <h5 className="mb-2 text-xl  font-bold tracking-tight text-gray-900 dark:text-white">The Complete 2023 Web Development
                             Bootcamp</h5>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">10 lessons</p>
                     </div>
