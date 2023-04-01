a50e3552f764ea96de5aa8bf88fe5cbf5eaa9892
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-04-01 17:57:51 +0530

    in community page whene user not loged in your community title is therebut data will not load , title removed , your community section can only access when user logedin bug fixed

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -58,31 +58,36 @@ function Community({ isTab }) {
 
         <div className={`w-full ${isTab ? "px-2 py-0" : "border-x px-5 py-3 sm:px-8"} border-base-300 `} >
             {/* community user have joinde */}
-            <div className='flex justify-between items-center mb-5'>
-                <h1 className='text-xl font-bold sm:text-2xl'>Your Community</h1>
-                {user.email ?
-                    <Button onClick={() => { setShowModal(true) }} >
-                        <div className="flex justify-center items-center"
-                            data-te-toggle="modal"
-                            data-te-target="#editCourse"
-                            data-te-ripple-init
-                            data-te-ripple-color="light">
-                            <BiPlus size={22} /> <span className='ml-4 hidden md:flex'>Create New</span>
+            {user.id? 
+            <>
+                    <div className='flex justify-between items-center mb-5'>
+                        <h1 className='text-xl font-bold sm:text-2xl'>Your Community</h1>
+                        {user.email ?
+                            <Button onClick={() => { setShowModal(true) }} >
+                                <div className="flex justify-center items-center"
+                                    data-te-toggle="modal"
+                                    data-te-target="#editCourse"
+                                    data-te-ripple-init
+                                    data-te-ripple-color="light">
+                                    <BiPlus size={22} /> <span className='ml-4 hidden md:flex'>Create New</span>
+                                </div>
+                            </Button>
+                            : null}
+                    </div>
+                    {loading ?
+                        <Loader />
+                        :
+                        <div className="mt-3 grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+                            {
+                                joinedCommunity.map((community) => (
+                                    <GroupCard key={community._id} community={community} joined={true} />
+                                ))
+                            }
                         </div>
-                    </Button>
-                    : null}
-            </div>
-            {loading ?
-                <Loader />
-                :
-                <div className="mt-3 grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
-                    {
-                        joinedCommunity.map((community) => (
-                            <GroupCard key={community._id} community={community} joined={true}  />
-                        ))
                     }
-                </div>
-            }
+            </>
+            :""}
+
 
             {/* list of all community */}
             <div className='pb-16'>
