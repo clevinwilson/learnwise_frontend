24350def37d64cf02d7dd92891049b24322f741e
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-04-12 16:09:08 +0530

    list group in community sidebar function added

--- src/components/CommunitySidebar/CommunitySidebar.jsx
+++ src/components/CommunitySidebar/CommunitySidebar.jsx
@@ -5,8 +5,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllJoinedGroups } from '../../Redux/Actions/groupActions';
 
 function CommunitySidebar() {
-    const groupData=useSelector(state=>state.group)
+    const dispatch=useDispatch();
+    const groupData=useSelector(state=>state.group);
    
+    useEffect(()=>{
+        dispatch(fetchAllJoinedGroups())
+    },[])
   return (
       <>
           <ul className="menu hidden sm:flex overflow-y-auto bg-base-100 text-base xl:w-64">
@@ -30,13 +34,13 @@ function CommunitySidebar() {
                   <h3 className="hidden px-5 py-2 text-xs font-bold uppercase text-gray-500 xl:block">
                       Groups
                   </h3>
-                  {groupData.groups.map((group,index) => (
+                  {groupData.groups && groupData.groups.map((group,index) => (
                       <li key={index}>
                           <div className="py-1">
                               <span className="mask mask-circle bg-base-200  text-xl">
                                   <img class="w-10 h-10 rounded-full" src={import.meta.env.VITE_SERVER_URL + group.image.path} alt="Rounded avatar"/>
                               </span>
-                              {/* <span className="hidden xl:block">{item.label}</span> */}
+                              <span className="hidden xl:block">{group.name}</span>
                           </div>
                       </li>
                   ))}
