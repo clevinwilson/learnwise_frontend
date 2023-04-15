8436b9d55036af8ecd8ffbba1ff6338e135dbb2b
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-04-15 10:25:59 +0530

    send message btn is not working some times fixed

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -105,7 +105,7 @@ function Messenger() {
                             <form onsubmit>
                                 <div className="relative">
                                     <label>
-                                        <input className="rounded-full py-2 pr-6 pl-10 w-full border border-gray-800 focus:border-gray-700 bg-gray-800 focus:bg-gray-900 focus:outline-none text-gray-200 focus:shadow-md transition duration-300 ease-in" type="text" defaultValue placeholder="Search Messenger" />
+                                        <input className="rounded-full py-2 pr-6 pl-10 w-full border border-gray-800 focus:border-gray-700 bg-gray-800 focus:bg-gray-900 focus:outline-none text-gray-200 focus:shadow-md transition duration-300 ease-in" type="text"  placeholder="Search Groups" />
                                         <span className="absolute top-0 left-0 mt-2 ml-3 inline-block">
                                             <svg viewBox="0 0 24 24" className="w-6 h-6">
                                                 <path fill="#bbb" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
@@ -172,10 +172,8 @@ function Messenger() {
                                             </button>
                                         </label>
                                     </div>
-                                    <button type="button" className="flex flex-shrink-0 focus:outline-none mx-2 h-9 w-9 bg-blue-500 text-white  justify-center items-center rounded-full">
-                                        <IoSend size={23}
-                                            onClick={handleSubmit}
-                                        />
+                                    <button onClick={handleSubmit} type="button" className="flex flex-shrink-0 focus:outline-none mx-2 h-9 w-9 bg-blue-500 text-white  justify-center items-center rounded-full">
+                                        <IoSend size={23}/>
                                     </button>
                                 </div>
                             </div>
