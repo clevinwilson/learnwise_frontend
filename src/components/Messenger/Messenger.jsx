3ac4a8c14f47843a0348a73e6f2ee43d6a7c8a19
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-04-18 21:30:43 +0530

    message ui updated

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -21,8 +21,11 @@ function Messenger() {
     const user = useSelector(state => state.user);
     const scrollRef = useRef();
     const socket = useRef();
-    const [showMessagesDiv, setshowMessagesDiv]=useState(true);
+    const [showMessagesDiv, setshowMessagesDiv] = useState(true);
     const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    
 
 
     //socket io
@@ -40,9 +43,9 @@ function Messenger() {
     //receive message and disconnect 
     useEffect(() => {
         //receive message
-        socket.current.on('receiveMessage', ({ userId, text }) => {
-            if (userId != user.id) {
-                setMessages(messages => [...messages, { sender: userId, text }]);
+        socket.current.on('receiveMessage', ({ sender, text }) => {
+            if (sender._id != user.id) {
+                setMessages(messages => [...messages, { sender: sender, text }]);
             }
         });
         // Clean up when the component unmounts
@@ -64,7 +67,8 @@ function Messenger() {
     useEffect(() => {
         getMessages(currentChat?._id)
             .then((response) => {
-                setMessages(response.data)
+                console.log(response.data);
+                setMessages(response.data);
             })
             .catch((err) => {
                 console.log(err);
@@ -100,28 +104,29 @@ function Messenger() {
         scrollRef.current?.scrollIntoView({ behavior: "smooth" })
     }, [messages]);
 
-
     return (
         <div className="h-screen w-full flex antialiased text-black bg-white overflow-hidden">
             <div className="flex-1 flex flex-col">
                 <main className="flex-grow border-t flex flex-row min-h-0">
-                    { showMessagesDiv ? 
-                    <section className="flex flex-col flex-none overflow-auto w-screen group border-r border-l border-[#dee2e7] lg:max-w-sm md:w-2/5 transition-all duration-300 ease-in-out">
-                        <div className="search-box border-b border-[#dee2e7] p-4 flex-none">
-                            <form >
-                                <div className="relative">
-                                    <label>
-                                        <input className="rounded-full py-2 pr-6 pl-10 w-full border border-gray-100 focus:border-gray-100 bg-gray-100 focus:bg-gray-100 focus:outline-none text-gray-800 focus:shadow-md transition duration-300 ease-in" type="text"  placeholder="Search Groups" />
-                                        <span className="absolute top-0 left-0 mt-2 ml-3 inline-block">
-                                            <svg viewBox="0 0 24 24" className="w-6 h-6">
-                                                <path fill="#bbb" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-                                            </svg>
-                                        </span>
-                                    </label>
-                                </div>
-                            </form>
-                        </div>
-                       {groupData.groups?
+                    {showMessagesDiv ?
+                        <section className="flex flex-col flex-none overflow-auto w-screen group border-r border-l border-[#dee2e7] lg:max-w-sm md:w-2/5 transition-all duration-300 ease-in-out">
+                            <div className="search-box border-b border-[#dee2e7] p-4 flex-none">
+                                <form >
+                                    <div className="relative">
+                                        <label>
+                                            <input className="rounded-full py-2 pr-6 pl-10 w-full border border-gray-100 focus:border-gray-100 bg-gray-100 focus:bg-gray-100 focus:outline-none text-gray-800 focus:shadow-md transition duration-300 ease-in" type="text" placeholder="Search Groups"
+                                                onChange={(event) => { setSearchTerm(event.target.value); }}
+                                             />
+                                            <span className="absolute top-0 left-0 mt-2 ml-3 inline-block">
+                                                <svg viewBox="0 0 24 24" className="w-6 h-6">
+                                                    <path fill="#bbb" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                                                </svg>
+                                            </span>
+                                        </label>
+                                    </div>
+                                </form>
+                            </div>
+                            {groupData.groups ?
                                 <div className="contacts flex-1 overflow-y-scroll">
                                     {groupData.groups && groupData.groups.map((group, index) => (
                                         <div key={index} onClick={() => { handleConversation(group) }}>
@@ -129,21 +134,21 @@ function Messenger() {
                                         </div>
                                     ))}
                                 </div>
-                        :
+                                :
                                 <div className='w-full h-full flex justify-center items-center'>
                                     No Joined Groups.
                                 </div>
-                        }
-                    </section>
+                            }
+                        </section>
                         : ""}
                     {currentChat ?
                         <section className="flex flex-col flex-auto pb-12 sm:pb-0">
                             <Chat isMobile={isMobile} setshowMessagesDiv={setshowMessagesDiv} currentChat={currentChat} />
                             <div className="chat-body p-4 flex-1 overflow-y-scroll">
-                                {currentChat && messages.map((message,index) => {
+                                {currentChat && messages.map((message, index) => {
                                     return (
                                         <div key={index} ref={scrollRef}>
-                                            <Message  message={message} currentChat={currentChat} own={user.id === message.sender._id} user={user} />
+                                            <Message message={message} currentChat={currentChat} own={user.id === message.sender._id} user={user} />
                                         </div>
                                     )
                                 })}
@@ -184,7 +189,7 @@ function Messenger() {
                                         </label>
                                     </div>
                                     <button onClick={handleSubmit} type="button" className="flex flex-shrink-0 focus:outline-none mx-2 h-9 w-9 bg-blue-600 text-white  justify-center items-center rounded-full">
-                                        <IoSend size={20}/>
+                                        <IoSend size={20} />
                                     </button>
                                 </div>
                             </div>
