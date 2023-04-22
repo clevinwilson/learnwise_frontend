413518118ec10c753e24bedb84f6cebaf3a60372
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-04-23 00:34:12 +0530

    course image is not listing error fixed

--- src/components/ViewCourses/ViewCourses.jsx
+++ src/components/ViewCourses/ViewCourses.jsx
@@ -173,7 +173,7 @@ function ViewCourses() {
                       </div>
                     </td>
                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                      <img src={`http://localhost:3000/${obj.image.path}`} width="80px" alt='Course Image' />
+                      <img src={import.meta.env.VITE_SERVER_URL +obj.image.path} width="80px" alt='Course Image' />
                     </th>
                     <td className="px-6 py-4">
                       {obj.name}
