735e5de8e290712d8fe17323cb86ed7e90d95ffd
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-06-24 10:26:35 +0530

    Home page course listing style updated to carousel

--- src/components/TopCourses/TopCourses.jsx
+++ src/components/TopCourses/TopCourses.jsx
@@ -4,32 +4,32 @@ import CourseCard from '../CourseCard/CourseCard';
 import Loader from '../Loader/Loader';
 
 function TopCourses() {
-  const [topCourse,setTopCourse]=useState([]);
-  const [loading,setLoading]=useState(true);
-  useEffect(()=>{
-    getTopCourse().then((response)=>{
-      if(response.data.status){
+  const [topCourse, setTopCourse] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getTopCourse().then((response) => {
+      if (response.data.status) {
         setTopCourse(response.data.courses);
         setLoading(false);
       }
     })
-  },[])
+  }, [])
   return (
     <div className='m-7 mt-10 mb-14'>
-          <h1 className='text-3xl font-semibold'>Top Courses</h1>
-
-          {loading?
-          <Loader/>
-          :
-        <div className='mt-10 grid-cols-1 gap-12  sm:grid-cols-2 grid md:grid-cols-3 xl:grid-cols-4 '>
+      <h1 className='text-3xl font-semibold mb-7'>Top Courses</h1>
+      {loading ?
+        <Loader />
+        :
+        <div className="carousel carousel-end rounded-box">
           {topCourse.map((course) => {
             return (
-              <CourseCard key={course._id} course={course} />
+              <div className="carousel-item mr-3 lg:mr-6">
+                <CourseCard key={course._id} course={course} />
+              </div>
             )
           })}
-
         </div>
-          }
+      }
     </div>
   )
 }
